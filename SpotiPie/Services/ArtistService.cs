2f59ed6fb16cbce6877790404cab5b698c31934b
NinPhi <melmarka4@gmail.com>
2024-03-15 20:33:38 +0600

    added follower feature

--- SpotiPie/Services/ArtistService.cs
+++ SpotiPie/Services/ArtistService.cs
@@ -17,7 +17,10 @@ public class ArtistService : IArtistService
 
     public async Task<ArtistGetDto?> GetByIdAsync(int id)
     {
-        var artist = await _dbContext.Artists.FindAsync(id);
+        var artist = await _dbContext
+            .Artists
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id);
 
         if (artist is null) return null;
 
@@ -34,7 +37,10 @@ public class ArtistService : IArtistService
 
     public async Task<List<ArtistGetDto>> GetAllAsync()
     {
-        var artists = await _dbContext.Artists.ToListAsync();
+        var artists = await _dbContext
+            .Artists
+            .AsNoTracking()
+            .ToListAsync();
 
         var artistDtos = artists.Select(a => new ArtistGetDto
         {
@@ -56,6 +62,7 @@ public class ArtistService : IArtistService
         };
 
         await _dbContext.Artists.AddAsync(artist);
+
         await _dbContext.SaveChangesAsync();
     }
 
@@ -66,6 +73,23 @@ public class ArtistService : IArtistService
         if (artist is null) return;
 
         _dbContext.Artists.Remove(artist);
+
+        //EntityEntry artistEntry = _dbContext.Entry(artist);
+        //artistEntry.State = EntityState.Deleted;
+
         await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<bool> AddFollowerAsync(int id)
+    {
+        var artist = await _dbContext.Artists.FindAsync(id);
+
+        if (artist is null) return false;
+
+        artist.Followers++;
+
+        await _dbContext.SaveChangesAsync();
+
+        return true;
     }
 }
