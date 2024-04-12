d46fb3dd2a715c6fd513ed95e8ad110f7874255f
NinPhi <melmarka4@gmail.com>
2024-04-12 20:42:20 +0600

    added swagger auth button

--- SpotiPie/Program.cs
+++ SpotiPie/Program.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SpotiPie.Middleware;
 using SpotiPie.Services;
-using System.Net;
 using System.Text;
 using System.Text.Json.Serialization;
 
@@ -15,7 +15,37 @@ builder.Services.AddControllers().AddJsonOptions(opts =>
 });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SpotiPie API",
+        Version = "v1"
+    });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+   {
+     new OpenApiSecurityScheme
+     {
+       Reference = new OpenApiReference
+       {
+         Type = ReferenceType.SecurityScheme,
+         Id = "Bearer"
+       }
+      },
+      new string[] { }
+    }
+  });
+});
+
 builder.Services.AddHttpContextAccessor();
 
 var securityKey = Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecurityKey"]!);
