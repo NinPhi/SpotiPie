fc28922867af36d4f66c4b89bcf6618ca0bff4c0
NinPhi <melmarka4@gmail.com>
2024-05-22 20:54:36 +0600

    added mvc

---
+++ SpotiPie.MVC/Controllers/ArtistsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpotiPie.Application.Contracts;
+using SpotiPie.Application.Services.Interfaces;
+
+namespace SpotiPie.MVC.Controllers;
+
+public class ArtistsController(IArtistService artistService) : Controller
+{
+    public async Task<IActionResult> Create()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> CreateForm(ArtistCreateDto artist)
+    {
+        await artistService.CreateAsync(artist);
+
+        return RedirectToAction("List");
+    }
+
+    public async Task<IActionResult> List()
+    {
+        var artists = await artistService.GetAllAsync();
+
+        return View(artists);
+    }
+
+    public async Task<IActionResult> DeleteForm(int id)
+    {
+        await artistService.DeleteAsync(id);
+
+        return RedirectToAction("List");
+    }
+}
---
+++ SpotiPie.MVC/Program.cs
@@ -0,0 +1,30 @@
+using SpotiPie.Application.Extensions;
+using SpotiPie.Infrastructure.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
